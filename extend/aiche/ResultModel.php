bf16a59c31b956f47880c82e655811de60cd3961
yuliang <313890735@qq.com>
2017-11-03 16:25:58 +0800

    修改ajaxAddCart中的to_catr为1，让页面直接跳转到cart2页面

---
+++ extend/aiche/ResultModel.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace   aiche ;
+
+class  ResultModel {
+    //代码
+    public $code = 1;
+
+    //消息
+    public $msg = "success";
+
+    //数据
+    public $data ;
+
+    public function resultData($data=null, $code=1, $msg="success"){
+        $this->code = $code;
+        $this->msg = $msg;
+        $this->data = $data;
+    }
+}
